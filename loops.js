739b1996da0b6e0ec5b8fd4d80133549962a5c3a
simvirk5 <simvirk5@gmail.com>
2018-03-20 23:03:19 -0700

    Commit 9 - Created loops.js file

---
+++ loops.js
@@ -0,0 +1,34 @@
+// var i = 0
+// while (i<=99) {
+// 	console.log(i++);
+// }
+
+// var i = 99
+// while (i>=0) {
+// 	console.log(i--);
+
+// }
+
+// for (var i = 0; i<=98; i+=2) {
+// 	console.log(i);
+// }
+
+// for (var i = 98; i>=0; i-=2) {
+// 	console.log(i);
+// }
+
+// var i = 49;
+// while(i<=72) {
+// 	console.log(i++);
+// }
+
+// var i = 81;
+// while(i>=26) {
+// 	console.log(i--);
+// }
+
+for(var i = 3; i<=90; i++) {
+	if(i%3===0) {
+		console.log(i);
+	}
+}
